ce6b9b4c7c69df16233220d5eefea8d6b08ad8ba
Shashank13021990 <shashankyadav.1990@gmail.com>
2019-07-24 13:45:26 +0100

    Updates

---
+++ precache-manifest.375448a72c079d692f35ef0fc4720e55.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2906019c1b751fcf52f0da2a37cf0e94",
+    "url": "/myreact/index.html"
+  },
+  {
+    "revision": "68a6919733bc8895f291",
+    "url": "/myreact/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "2e0129063922e3bb197a",
+    "url": "/myreact/static/js/2.b41502e9.chunk.js"
+  },
+  {
+    "revision": "68a6919733bc8895f291",
+    "url": "/myreact/static/js/main.ec392c14.chunk.js"
+  },
+  {
+    "revision": "a1a1d8a90cb3c44363e9",
+    "url": "/myreact/static/js/runtime~main.b769e414.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/myreact/static/media/logo.5d5d9eef.svg"
+  }
+]);
